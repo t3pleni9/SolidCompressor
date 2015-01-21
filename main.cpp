75ff0d84a3be7610f3edbd76a33e2a33f21284fd
Tim <tim@archbox.com>
2015-01-21 10:04:45 +0530

    Test commit for archbox

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
         printf("%d", 1000);
         char* pt;
         char a[] ={"Help me mofo"};
-        strcpy(pt, a);
+        //strcpy(pt, a);
         printf("%u %u", (int)a, (int)pt);
 }
